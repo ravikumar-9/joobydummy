dc71f94352029e0af2818513982c48a4c2570003
ravikumar-9 <ravikumarravikumar32439@gmail.com>
2023-07-11 07:40:00 +0000

    commit message

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,12 @@ import {withRouter, Link} from 'react-router-dom'
 
 import Cookies from 'js-cookie'
 
+import {AiFillHome} from 'react-icons/ai'
+
+import {BsFillBriefcaseFill} from 'react-icons/bs'
+
+import {FiLogOut} from 'react-icons/fi'
+
 import './index.css'
 
 const Header = props => {
@@ -14,26 +20,57 @@ const Header = props => {
   }
 
   return (
-    <nav className="header-section">
-      <Link to="/" className="nav-link">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
-          alt="website logo"
-          className="web-logo"
-        />
-      </Link>
-      <ul className="menu-container">
-        <Link to="/" className="nav-link">
-          <li className="menu-item">Home</li>
-        </Link>
-        <Link to="/jobs" className="nav-link">
-          <li className="menu-item">Jobs</li>
-        </Link>
-      </ul>
-      <button className="logout-button" type="button" onClick={onLogout}>
-        Logout
-      </button>
-    </nav>
+    <>
+      <div className="large-devices">
+        <nav className="header-section">
+          <Link to="/" className="nav-link">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+              alt="website logo"
+              className="web-logo"
+            />
+          </Link>
+          <ul className="menu-container">
+            <Link to="/" className="nav-link">
+              <li className="menu-item">Home</li>
+            </Link>
+            <Link to="/jobs" className="nav-link">
+              <li className="menu-item">Jobs</li>
+            </Link>
+          </ul>
+          <button className="logout-button" type="button" onClick={onLogout}>
+            Logout
+          </button>
+        </nav>
+      </div>
+      <div className="small-devices">
+        <nav className="mobile-device-nav-header">
+          <Link to="/" className="nav-link">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+              className="mob-device-web-logo"
+              alt="website logo"
+            />
+          </Link>
+          <div className="mob-device-menu-container">
+            <Link to="/" className="nav-link">
+              <AiFillHome color="white" className="mob-device-home-img" />
+            </Link>
+            <Link to="/jobs" className="nav-link">
+              <BsFillBriefcaseFill
+                color="white"
+                className="mob-device-home-img"
+              />
+            </Link>
+            <FiLogOut
+              color="white"
+              className="mob-device-home-img"
+              onClick={onLogout}
+            />
+          </div>
+        </nav>
+      </div>
+    </>
   )
 }
 
